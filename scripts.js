4124ee0065046659e7caf21fb27e03f635e1d255
1jakerobertson <jacob.robertson@bison.howard.edu>
2026-03-04 13:37:59 -0500

    function appendOperator() implementation

--- scripts.js
+++ scripts.js
@@ -23,12 +23,14 @@ function appendNumber(number) {
   console.log(number);
 }
 
+// clear display
 function clearDisplay() {
   displayValue = "";
   updateDisplay();
   console.log("Display cleared");
 }
 
+// toggle sign
 function toggleSign() {
   if (displayValue === "") return;
   displayValue = displayValue * -1;
@@ -36,9 +38,22 @@ function toggleSign() {
   console.log("Sign toggled");
 }
 
+// percent
 function percent() {
   if (displayValue === "") return;
   displayValue = displayValue / 100;
   updateDisplay();
   console.log("Percent toggled");
+}
+
+// append operator
+function appendOperator(operator) {
+  if (
+    displayValue !== "" &&
+    !isNaN(displayValue.charAt(displayValue.length - 1))
+  ) {
+    displayValue += operator;
+    updateDisplay();
+    console.log(operator + " appended");
+  }
 }
