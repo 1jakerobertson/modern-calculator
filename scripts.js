887108e3ba4b79695c187f89ff38b6237c3c3978
1jakerobertson <jacob.robertson@bison.howard.edu>
2026-03-04 01:23:32 -0500

    Modern calculator, coding challenge, now comes logic

---
+++ scripts.js
@@ -0,0 +1,32 @@
+function multiply(number, secondNumber) {
+    return number * secondNumber;
+}
+
+function addition(number, secondNumber) {
+    return number + secondNumber;
+}
+
+function subtraction(number, secondNumber) {
+    return number - secondNumber;
+}
+
+function division(number, secondNumber) {
+    return number / secondNumber;
+}
+
+
+/*function calculator (number, secondNumber, operator) {
+  if (operator === 'multiply') {
+    return multiply(number, secondNumber);
+  } else if (operator === 'addition') {
+    return addition(number, secondNumber);
+  } else if (operator === 'subtraction') {
+    return subtraction(number, secondNumber);
+  } else if (operator === 'division') {
+    return division(number, secondNumber);
+  } else {
+    return 'Invalid operator';
+  }
+}*/
+
+console.log(multiply(5, 10));
